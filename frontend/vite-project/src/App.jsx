d8b416a640d71d26537e86692ae9c547a6da8d22
sangamgowdahm <159030690+sangamgowdahm@users.noreply.github.com>
2025-06-02 19:04:37 +0530

    Update App.jsx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -11,7 +11,7 @@ import SignUp from './pages/Auth/SignUp';
 import Home from './pages/dashboard/Home';
 import Income from './pages/dashboard/Income';
 import Expense from './pages/dashboard/Expense';
-import UserProvider from './context/UserContext';
+import UserProvider from './context/userContext';
 import { Toaster } from 'react-hot-toast';
 
 
